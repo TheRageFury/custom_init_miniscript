9aab0d11b54515489d7c416d53ed13541052b398
TheRageFury <62329316+TheRageFury@users.noreply.github.com>
2022-03-17 14:53:44 +0100

    Updated installer and uninstaller
    Fixed some issue and finally completed the 1st release

--- custom_init_files/uninstaller.sh
+++ custom_init_files/uninstaller.sh
@@ -6,7 +6,7 @@ if [ "$1" == "" ]
 then
         echo "[custom_init] ERROR: You must provide an argument specifying the user for which remove custom_init!"
         echo "[custom_init] Deletion ABORTED. Exiting ..."
-        return
+        exit 1
 fi
 
 normal_user=$1
@@ -15,7 +15,7 @@ curr_user_id=$(id -u)
 if [ "$curr_user_id" != "0" ]
 then
         echo "[custom_init] ERROR: You must run this uninstaller script as ROOT. Try using sudo before actual executing command"
-        return
+        exit 2
 fi
 
 user_home="/home/${normal_user}"
@@ -23,6 +23,7 @@ ci_path="${user_home}/custom_init"
 bashrc_path="${user_home}/.bashrc"
 
 rm -rf ${ci_path}
-sed "'/custom_init/d'" ${user_home}/.bashrc
+sed -i '/custom_init/d' ${user_home}/.bashrc
+sed -i '/debug_opt=/d' ${user_home}/.bashrc
 
 echo "[custom_init] custom_init SCRIPT UNINSTALLED! To complete the procedure remove this .sh script manually"
--- custom_init_installer.sh
+++ custom_init_installer.sh
@@ -6,7 +6,7 @@ if [ "$1" == "" ]
 then
 	echo "[custom_init] ERROR: You must provide an argument specifying the user for which install custom_init!"
 	echo "[custom_init] Installation ABORTED. Exiting ..."
-	return
+	exit 1
 fi
 
 normal_user=$1
@@ -15,42 +15,45 @@ curr_user_id=$(id -u)
 if [ "$curr_user_id" != "0" ] 
 then
 	echo "[custom_init] ERROR: You must run this installer script as ROOT. Try using sudo before actual executing command"
-	return
+	exit 2
 fi
+
+echo "[custom_init] Setting start/end message and hooker loading in .bashrc file ..."
 
 user_home="/home/${normal_user}"
 bashrc_path="${user_home}/.bashrc"
 
 start_message="echo \"[.bashrc] Running r4g3's custom_init.sh ...\""
-end_message="echo \"[.bashrc] r4g3's custom_init.sh: DONE!\""
+end_message="echo \"[.bashrc] r4g3's custom_init.sh: DONE! \""
 
 echo "$start_message" >> $bashrc_path
 echo "debug_opt=0" >> $bashrc_path
-echo "source ${user_home}/custom_init/custom_init.sh \$debug_opt"
+echo "source ${user_home}/custom_init/custom_init.sh \$debug_opt" >> $bashrc_path
 echo "$end_message" >> $bashrc_path
 
-cp -r ./custom_init_files/custom_init ${user_home}/
+echo "[custom_init] .bashrc HOOKER CODE SET!"
+echo "[custom_init] Copying the script folder with the main loader and some scripts ..."
 
 ci_path="${user_home}/custom_init"
+installer_path=$(dirname "${BASH_SOURCE[0]}")
+
+cp -r ${installer_path}/custom_init_files/custom_init ${user_home}/
 
 chmod 755 ${ci_path}/custom_init.sh
 chown ${normal_user}:${normal_user} ${ci_path}/custom_init.sh
 
-##
-## TODO: Modify this part of the installer to be dynamic referring to a list of files
-##
+chmod -R 755 ${ci_path}
+chown -R ${normal_user}:${normal_user} ${ci_path}
 
-chmod 755 ${ci_path}/xampp/loader.sh
-chown ${normal_user}:${normal_user} ${ci_path}/xampp/loader.sh
-
-chmod 755 ${ci_path}/xampp/aliases.sh
-chown ${normal_user}:${normal_user} ${ci_path}/xampp/aliases.sh
+echo "[custom_init] FILES AND LOADER (custom_init.sh) COPIED!"
+echo "[custom_init] Creating uninstaller ..."
 
 touch ${user_home}/ci_uninstall.sh
-cat ./custom_init_files/uninstaller.sh >> ${user_home}/ci_uninstall.sh
+cat ${installer_path}/custom_init_files/uninstaller.sh >> ${user_home}/ci_uninstall.sh
 chmod 700 ${user_home}/ci_uninstall.sh
 chown ${normal_user}:${normal_user} ${user_home}/ci_uninstall.sh
 
+echo "[custom_init] UNINSTALLER CREATED!"
 echo "[custom_init] SCRIPT INSTALLED! Uninstaller created at: ${user_home}/ci_uninstall.sh"
 
 source ${user_home}/.bashrc
